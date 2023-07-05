7b6f17b7380f8e7b0dc7bfb07e75dc9e80ea6368
AleksandraV108 <69168481+AleksandraV108@users.noreply.github.com>
2023-07-05 21:28:38 +0200

    Delete animation.js

--- js/animation.js
+++
@@ -1,6 +0,0 @@
-gsap.registerPlugin(ScrollTrigger);
-
-let tl = gsap.timeline({Defaults: { Easing: Expo.EaseOut}});
-     tl.from('.bg',{ opacity:0, duration:2, ease: Expo.easeOut,delay: 0.2 })
-       .to('.text-reveal',{ opacity:1, stagger: .3, duration: 1.5}, "-=1.5")
-       .to('.image-reveal', { clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',y:0, stagger: 1, opacity: 1, duration: 1} )
